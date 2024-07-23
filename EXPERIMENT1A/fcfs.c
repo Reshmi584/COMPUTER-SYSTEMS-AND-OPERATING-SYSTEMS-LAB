39508ba8b766b31fd3928622cb52a1fadc924796
224g1A0584 <151389249+Reshmi584@users.noreply.github.com>
2024-07-23 22:52:02 +0530

    Add files via upload

---
+++ EXPERIMENT1A/fcfs.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void main()
+{
+    int bt[20], wt[20], tat[20], i, n;
+    float wtavg, tatavg;
+    int clrscr();
+    printf("\nEnter the number of processes -- ");
+    scanf("%d", &n);
+    for(i=0;i<n;i++)
+    {
+        printf("\nEnter Burst process %d -- ", i);
+        scanf("%d", &bt[i]);
+    }
+    wt[0] = wtavg = 0;
+    tat[0] = tatavg = bt[0];
+    for(i=1;i<n;i++)
+    {
+        wt[i] = wt[i-1] +bt[i-1];
+        tat[i] = tat[i-1] +bt[i];
+        wtavg = wtavg + wt[i];
+        tatavg = tatavg + tat[i];
+    }
+    printf("\t\tBURST TIME \t WT\t TAT\n");
+    for(i=0;i<n;i++)
+        printf("\n\t\t\t %d \t\t %d \t\t %d", bt[i], wt[i], tat[i]);
+    printf("\nwtavg -- %f", wtavg/n);
+    printf("\ntatavg -- %f", tatavg/n);
+    int getch();
+}
